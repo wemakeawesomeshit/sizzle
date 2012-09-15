d6229edea53421d5c455b9a9d618495562ae9dda
Syd Lawrence <sydlawrence@gmail.com>
2012-09-15 10:42:58 +0100

    added getUserMedia

--- fish/js/script.js
+++ fish/js/script.js
@@ -2,8 +2,33 @@
 
 */
 
+if (navigator.getUserMedia) {}
+else if (navigator.webkitGetUserMedia) { navigator.getUserMedia = navigator.webkitGetUserMedia}
+else if (navigator.mozGetUserMedia) { navigator.getUserMedia = navigator.mozGetUserMedia}
+else if (navigator.msGetUserMedia) { navigator.getUserMedia = navigator.msGetUserMedia}
 
 
+
+
+$(document).ready(function() {
+
+	 var onFailSoHard = function(e) {
+	    console.log('Reeeejected!', e);
+	  };
+
+	  // Not showing vendor prefixes.
+	  navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+	    var video = document.getElementById('webcam');
+	    video.src = window.URL.createObjectURL(localMediaStream);
+
+	    // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+	    // See crbug.com/110938.
+	    video.onloadedmetadata = function(e) {
+	      // Ready to go. Do some stuff.
+	    };
+	  }, onFailSoHard);
+
+})
 
 
 
